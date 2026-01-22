308d6574a535ee45b36db14a4c299f3957dd2291
Shubham Ingale <shubhingale23052004@gmail.com>
2026-01-22 14:40:04 +0530

    Fix- The schema of fronend is fixed and the correct table structure is Created to display backend data.

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -5,6 +5,7 @@ import { validateExcelFile } from "../utils/validateFile";
 const FileUpload = () => {
     const [file, setFile] = useState(null);
     const [message, setMessage] = useState("");
+    const [rows, setRows] = useState([]);
     const [loading, setLoading] = useState(false);
     const handleUpload = async () => {
         const error = validateExcelFile(file);
@@ -18,7 +19,14 @@ const FileUpload = () => {
 
         try {
             setLoading(true);
-            const res = await api.post("/uploadExcel", formData);
+            const res = await api.post("/api/uploadExcel", formData);
+            const formattedRows=res.data.data.rows.map((row)=>({
+                name:row.Name,
+                email:row.Email,
+                age:row.Age
+
+            }));
+            setRows(formattedRows);
             setMessage(res.data.message);
         } catch (err) {
             setMessage("Upload failed");
@@ -42,6 +50,29 @@ const FileUpload = () => {
             </button>
 
             <p>{message}</p>
+
+            {rows.length>0 && (
+                <table border="1" cellPadding="10" style={{ marginTop: "20px" }}>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {rows.map((row,index)=>(
+                        <tr key={index}>
+                            <td>{row.name}</td>
+                            <td>{row.email}</td>
+                            <td>{row.age}</td>
+                        </tr>
+                            )
+                        )
+                    }
+                    </tbody>
+                </table>
+            )}
         </div>
     );
 };
