664d189397b009b75292e089dbf0372c4d895f86
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-06 17:35:39 +0530

    Feat: Creating a componentfor top5 regions table.

---
+++ src/components/Tables/top5RegionsTable.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+
+const Top5RegionsTable = () => {
+    const [data, setData] = useState([]);
+    const stickyTh = {
+        position: "sticky",
+        top: 0,
+        background: "#f8f9fa",
+        zIndex: 2,
+        fontSize: "0.75rem" // Smaller header text
+    };
+
+    useEffect(() => {
+        fetch("http://localhost:5000/api/top5regionsTable")
+            .then(res => res.json())
+            .then(res => {
+                if (!res.success) return;
+                setData(res.data);
+            });
+    }, []);
+
+    return (
+        <div className="card shadow-sm rounded-4" style={{ maxWidth: "400px" }}>
+            <div className="card-body p-2">
+                <h6 className="text-center fw-bold text-secondary mb-1" style={{ fontSize: "0.85rem" }}>
+                    Top 5 Regions
+                </h6>
+
+                <div className="table-responsive" style={{ maxHeight: "170px" }}>
+                    <table className="table table-sm table-bordered align-middle text-center mb-0">
+                        <thead className="table-light">
+                        <tr>
+                            <th style={{...stickyTh, width: "60px"}}>Sr.</th>
+                            <th style={{...stickyTh, minWidth: "120px"}}>RegionName</th>
+                            <th style={{...stickyTh, width: "100px"}}>Count</th>
+                        </tr>
+                        </thead>
+
+                        <tbody style={{ fontSize: "0.8rem" }}>
+                        {data.length > 0 ? (
+                            data.map((item, index) => (
+                                <tr key={index}>
+                                    <td className="fw-semibold">{index + 1}</td>
+                                    <td className="fw-normal text-center px-2" style={{ fontSize: "0.80rem" }}>
+                                        {item.regionName}
+                                    </td>
+                                    <td className="fw-bold text-primary">{item.count.toLocaleString()}</td>
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan="3" className="text-center text-muted py-2" style={{ fontSize: "0.75rem" }}>
+                                    Loading...
+                                </td>
+                            </tr>
+                        )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Top5RegionsTable;
