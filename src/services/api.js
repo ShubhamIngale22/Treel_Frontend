14d10186040ae05bb59a1bf3c7ab2d88d48411cc
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-10 14:43:42 +0530

    Refactor: Creating function to fetch data from API.

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,30 @@
-import axios from "axios";
+const BASE_URL = "http://localhost:5000/api";
+const fetchApi = async (endpoint) => {
+    const res = await fetch(`${BASE_URL}${endpoint}`);
+    const data = await res.json();
+    return data;
+};
 
-const api=axios.create({
-    baseURL: "http://localhost:5000",
-});
-export default api;
+module.exports={
+    getInstallation:()=>
+        fetchApi("/installationTable"),
+
+    getTop5Dealer:()=>
+        fetchApi("/top5DealerTable"),
+
+    getTop5MakeModel:()=>
+        fetchApi("/top5MakeModelTable"),
+
+    getTop5Regions:()=>
+        fetchApi("/top5regionsTable"),
+
+    getTop5Zones:()=>
+        fetchApi("/top5ZonesTable"),
+
+    getDealerInstallations:(range)=>
+        fetchApi(`/installationsLineChart?type=${range}`),
+
+    getZoneWisePieChart:(metric,range)=>
+        fetchApi(`/zone-installations?type=${range}&metric=${metric}`)
+
+};
