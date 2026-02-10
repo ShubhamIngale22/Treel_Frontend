7d14d7d58622da8f99d4eb3d51a5e6d438c702e9
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-10 14:55:25 +0530

    Refactor: Commenting code of fileupload and uploadpage beacause as of now it is no useful.

--- src/Pages/UploadPage.jsx
+++ src/Pages/UploadPage.jsx
@@ -1,12 +1,12 @@
-import FileUpload from "../components/FileUpload";
-// import "../styles/uploadPage.css";
-
-const UploadPage = () => {
-    return (
-        <>
-            <FileUpload />
-        </>
-    );
-};
-
-export default UploadPage;
+// import FileUpload from "../components/FileUpload";
+// // import "../styles/uploadPage.css";
+//
+// const UploadPage = () => {
+//     return (
+//         <>
+//             <FileUpload />
+//         </>
+//     );
+// };
+//
+// export default UploadPage;
