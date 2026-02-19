b4b028de86d4a8cf7dafe5d2425851ad38993391
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-19 15:41:50 +0530

    Refactor: Shifted project from CRA (Create react app) to react-vite.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import UploadPage from "./Pages/UploadPage";
 import DashBoard from "./Pages/DashBoard";
 import "./components/charts/chartConfig";
 
 function App() {
   return (
     <>
-        {/*<UploadPage/>*/}
         <DashBoard/>
     </>
   )
